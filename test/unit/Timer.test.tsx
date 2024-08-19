d72bae357a536ee092ffa99295723006218485c5
LinkTheCoder <66062268+LinkTheCoder@users.noreply.github.com>
2024-08-19 15:19:54 +0200

    updated unit test to check render of time & buttons

--- test/unit/Timer.test.tsx
+++ test/unit/Timer.test.tsx
@@ -1,10 +1,21 @@
-import { describe, it } from 'vitest';
-import { render } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import Timer from '../../components/mindfulness/meditation/Timer';
+import '@testing-library/jest-dom/vitest';
 
-describe('Timer', () => {
-  it('renders without crashing', () => {
+describe('Timer Component', () => {
+  it('renders timer display with initial value and the buttons', () => {
     render(<Timer />);
+    
+    // Check timer display
+    expect(screen.getByTestId('minutes').textContent).toBe('00');
+    expect(screen.getByTestId('seconds').textContent).toBe('00');
+    expect(screen.getByTestId('tens').textContent).toBe('00');
+    
+    // Check buttons
+    expect(screen.getByTestId('play')).toBeInTheDocument();
+    expect(screen.getByTestId('pause')).toBeInTheDocument();
+    expect(screen.getByTestId('backward')).toBeInTheDocument();
   });
 });
